314e9c46cccd82aec15aa536b125bc0b0b53a131
Kefircheggg <vnikifor31@gmail.com>
2025-02-26 13:08:29 +0700

    программа-меню для выбора задачи

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Выберите задачу:" << endl;
+    cout << "1. Является ли строка палиндромом" << endl;
+    cout << "2. Удалить спецсимволы из строки" << endl;
+    int choice;
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            cout << "keker";
+            break;
+        case 2:
+            cout << "loler";
+            break;
+        default:
+            cout << "Неизвестная команда";
+    }
     return 0;
 }
 
