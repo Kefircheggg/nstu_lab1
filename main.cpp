d3090ef07da3a6fa5f2222ad94cea9bf3b222467
Artem <artemkamix1@yandex.ru>
2025-02-26 13:13:15 +0700

    wow it works

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+//
+// Created by Артем Скороходов on 26.02.2025.
+//
+
 #include <iostream>
+#include <string>
+using namespace std;
 
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+bool isSymb(char ch){
+    string symbols = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+    for (int i = 0; i < symbols.size(); i++) {
+        if (symbols[i] == ch) {
+            return true;
+        }
+    }
+    return false;
 }
 
+string delSpecials(string s){
+    string result = "";
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ' || isSymb(s[i])) {
+            result += s[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+    string s = "";
+    cout<<"Enter a string: ";
+    getline(cin, s);
+    string result = delSpecials(s);
+    cout<<result;
+}
