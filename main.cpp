da8d86a073692669dcc5cb64379078e79080fee6
Kefircheggg <vnikifor31@gmail.com>
2025-02-26 12:44:06 +0700

    impl isPalindrome func

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+using namespace std;
 
+bool isPalindrome(string &s) {
+    int len = s.length();
+    for (int i = 0; i < len / 2; i++) {
+        if (s[i] != s[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Введите строку" << endl;
+    string input;
+    cin >> input;
+    string output = isPalindrome(input) ? "Строка - палиндром" : "Строка - не палиндром";
+    cout << output << endl;
     return 0;
 }
 
