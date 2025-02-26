6ec0077924ad154f91153d52fdd695c7d40e7186
Kefircheggg <vnikifor31@gmail.com>
2025-02-26 13:14:17 +0700

    add endl to become better programmer

--- main.cpp
+++ main.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 int main() {
-    cout << "Выберите задачу:" << endl;
-    cout << "1. Является ли строка палиндромом" << endl;
-    cout << "2. Удалить спецсимволы из строки" << endl;
-    int choice;
-    cin >> choice;
-    switch (choice) {
-        case 1:
-            cout << "keker";
-            break;
-        case 2:
-            cout << "loler";
-            break;
-        default:
-            cout << "Неизвестная команда";
+    while (true) {
+        cout << "Выберите задачу:" << endl;
+        cout << "1. Является ли строка палиндромом" << endl;
+        cout << "2. Удалить спецсимволы из строки" << endl;
+        int choice;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "keker" << endl;
+                break;
+            case 2:
+                cout << "loler" << endl;
+                break;
+            default:
+                cout << "Неизвестная команда" << endl;
+        }
     }
+
     return 0;
 }
 
