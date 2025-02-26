8d789fb01a84b142ceb47c0401a4fa5a75d59bc0
Kefircheggg <vnikifor31@gmail.com>
2025-02-26 18:49:32 +0700

    добавил комментарии к части кода

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 bool isPalindrome(string &s) {
-    int len = s.length();
-    for (int i = 0; i < len / 2; i++) {
-        if (s[i] != s[len - i - 1]) {
-            return false;
+    int strLen = s.length();
+    for (int i = 0; i < strLen / 2; i++) { //прогоняем все индексы до середины строки
+        if (s[i] != s[strLen - i - 1]) { //если i-й символ с левого и правого концов не совпадают
+            return false; //то строка не является палиндромом:(
         }
     }
     return true;
 }
 
-bool isSymb(char ch){
+bool isSymb(char ch) {
     string symbols = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэёячсмитьбю";
     for (int i = 0; i < symbols.size(); i++) {
         if (symbols[i] == ch) {
@@ -33,30 +33,33 @@ string delSpecials(string s) {
 }
 
 int main(){
-    while (true) {
+    while (true) { //можно гонять задачки сколько хочется без перезапуска
         cout << "Выберите задачу:" << endl;
         cout << "1. Является ли строка палиндромом" << endl;
         cout << "2. Удалить спецсимволы из строки" << endl;
         cout << "3. Выход" << endl;
         int choice;
-        cin >> choice;
+        cin.clear(); //защита от поломки в случае ввода текста вместо числа
+        cin.ignore(512,'\n');
+        cin >> choice; //программа-меню (так в первом семестре говорили)
         switch (choice) {
             case 1: {
                 string input;
-                cout << "Введите строку" << endl;
+                cout << "Введите строку: ";
                 cin >> input;
-                string output = (isPalindrome(input) ? "Строка - палиндром" : "Строка - не палиндром");
+                bool checkResult = isPalindrome(input);
+                string output = checkResult ? "Строка - палиндром" : "Строка - не палиндром"; //ответ через тернарный оператор
                 cout << output << endl;
-            }
+                }
                 break;
             case 2: {
-                string s = "";
+                string input;
                 cout << "Введите строку: ";
-                cin.ignore();  // Добавляем игнорирование символа новой строки
-                getline(cin, s);
-                string result = delSpecials(s);
-                cout << result << endl;
-            }
+                cin.ignore();  // Добавляем игнорирование символа новой строки чтобы getline нормально работал
+                getline(cin, input); //getline вместо cin потому что cin работает до пробела
+                string output = delSpecials(input);
+                cout << output << endl;
+                }
                 break;
             case 3:
                 cout << "Пока!" << endl;
@@ -65,6 +68,5 @@ int main(){
                 cout << "Неизвестная команда" << endl;
         }
     }
-
     return 0;
 }
